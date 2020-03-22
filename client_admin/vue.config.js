f9c5adbb74e2c88d9297b3123199b43fbdbb4398
KevinTang <tjtzc@126.com>
2020-03-22 22:19:32 +0800

    progressing

--- client_admin/vue.config.js
+++ client_admin/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://192.168.199.214:3000',
+        target: 'http://localhost:3000',
         changeOrigin: true
       },
     }
